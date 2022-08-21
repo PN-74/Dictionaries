cc14c5a0adb5e7abfe5ab283f4f3094f57b9d132
Pooja nawale <111230450+PN-74@users.noreply.github.com>
2022-08-21 15:22:30 +0530

    Add files via upload

---
+++ Untitled37.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+asciidict = dict()
+alphabetletter = range(97,123)
+for i in alphabetletter:
+    asciidict[str(i)] = chr(i)
+    print(asciidict)
+
+
+# In[ ]:
+
+
+
+
